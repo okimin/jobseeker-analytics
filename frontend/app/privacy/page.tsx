ed622f9fbac0b2928d6f1129ebd688470bb909ab
Lianna Novitz <32498202+lnovitz@users.noreply.github.com>
2025-12-25 23:37:25 -0800

    lint

--- frontend/app/privacy/page.tsx
+++ frontend/app/privacy/page.tsx
@@ -335,8 +335,8 @@ const PrivacyPolicyPage = () => {
 					href="https://developers.google.com/terms/api-services-user-data-policy"
 				>
 					Google API Services User Data Policy
-				</a>
-				{" "}including Limited Use requirements.
+				</a>{" "}
+				including Limited Use requirements.
 			</p>
 			<ul className="list-disc list-inside space-y-2 mt-2 font-medium text-sm">
 				<li>NO Human Access to your data unless explicitly authorized by you for support.</li>
