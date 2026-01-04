d235a5429d8b4aac80edd6b91045df73ecb8d347
Lianna Novitz <32498202+lnovitz@users.noreply.github.com>
2026-01-03 18:04:15 -0800

    add local dev bypass

--- backend/utils/billing_utils.py
+++ backend/utils/billing_utils.py
@@ -3,14 +3,18 @@ import logging
 import stripe
 from fastapi.concurrency import run_in_threadpool
 
-from utils.config_utils import get_stripe_key
+from utils.config_utils import get_stripe_key, get_settings
 
 # Logger setup
 logger = logging.getLogger(__name__)
 
+settings = get_settings()
 
 async def check_promo_is_valid(user_input_promo: str) -> bool:
     user_input_promo = user_input_promo.strip()[:16]
+    if not settings.is_publicly_deployed:
+        logger.info("Local dev detected: Bypassing Stripe promo check.")
+        return True
     get_stripe_key()
     try:
         promo = await run_in_threadpool(
