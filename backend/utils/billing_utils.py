65f5ac473dd0a30e573f24b6178e1298579c8030
Lianna Novitz <32498202+lnovitz@users.noreply.github.com>
2026-01-03 17:08:00 -0800

    add await

--- backend/utils/billing_utils.py
+++ backend/utils/billing_utils.py
@@ -13,7 +13,7 @@ async def check_promo_is_valid(user_input_promo: str) -> bool:
     user_input_promo = user_input_promo.strip()[:16]
     get_stripe_key()
     try:
-        promo = stripe.PromotionCode.list(code=user_input_promo, active=True)
+        promo = await stripe.PromotionCode.list(code=user_input_promo, active=True)
         return len(promo.data) > 0
     except stripe.error.StripeError as e:
         logger.error("Error retrieving promocode %s" % e)
