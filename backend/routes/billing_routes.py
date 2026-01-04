7c69e536a1e0a6057fd29e9fece42822489f9a60
Lianna Novitz <32498202+lnovitz@users.noreply.github.com>
2026-01-03 17:07:09 -0800

    raise exception if invalid

--- backend/routes/billing_routes.py
+++ backend/routes/billing_routes.py
@@ -1,5 +1,5 @@
 import logging
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Request, HTTPException
 from slowapi import Limiter
 from slowapi.util import get_remote_address
 from utils.config_utils import get_settings
@@ -14,12 +14,15 @@ logger = logging.getLogger(__name__)
 router = APIRouter()
 limiter = Limiter(key_func=get_remote_address)
 
+
 @router.get("/billing/promos/{promo_id}", response_model=bool)
 @limiter.limit("10/hour")
 async def check_valid_promo(request: Request, promo_id: str):
     try:
-        response = await check_promo_is_valid(promo_id)
+        is_valid = await check_promo_is_valid(promo_id)
+        if not is_valid:
+            raise HTTPException(status_code=400, detail="Invalid code")
+        return {"status": "success"}
     except Exception as e:
-        response = False
-        logger.error("Issue checking promo code")
-    return response
+        logger.error("check_valid_promo error %s" % e)
+        raise HTTPException(status_code=500, detail="Something went wrong")
--- backend/utils/billing_utils.py
+++ backend/utils/billing_utils.py
@@ -8,13 +8,13 @@ from utils.config_utils import get_stripe_key
 # Logger setup
 logger = logging.getLogger(__name__)
 
-def check_promo_is_valid(user_input_promo: str) -> bool:
+
+async def check_promo_is_valid(user_input_promo: str) -> bool:
     user_input_promo = user_input_promo.strip()[:16]
     get_stripe_key()
     try:
-        payload = stripe.PromotionCode.list(code=user_input_promo, active=True)
-        promo = json.loads(payload).get("data")
-        return len(promo) > 0
+        promo = stripe.PromotionCode.list(code=user_input_promo, active=True)
+        return len(promo.data) > 0
     except stripe.error.StripeError as e:
         logger.error("Error retrieving promocode %s" % e)
     return False
