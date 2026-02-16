03046bb81865bc5a64a83ef31b9b9c44fc02269d
Lianna Novitz <32498202+lnovitz@users.noreply.github.com>
2026-02-15 20:54:55 -0800

    use nonce for styles

--- frontend/proxy.ts
+++ frontend/proxy.ts
@@ -11,7 +11,7 @@ export function proxy(request: NextRequest) {
 	const cspHeader = `
     default-src 'self';
     script-src 'self' 'nonce-${nonce}' 'strict-dynamic' https://apis.google.com https://accounts.google.com;
-    style-src 'self' 'unsafe-inline' https://app.termly.io;
+    style-src 'self' 'nonce-${nonce}' https://app.termly.io;
     img-src 'self' blob: data: https://us-assets.i.posthog.com https://app.termly.io;
     connect-src 'self' http://localhost:8000 https://us.i.posthog.com api.justajobapp.com https://app.termly.io https://*.api.termly.io;
     frame-src 'self' https://app.termly.io https://www.youtube.com;
